fe4092fc0c5dbbb3762a03361ce7700f851e0d7d
giorgi-ghviniashvili <mr.g.ghv@gmail.com>
2018-01-11 10:45:07 +0400

    update
    update

--- buy-or-rent/scripts/d3script.js
+++ buy-or-rent/scripts/d3script.js
@@ -5,33 +5,11 @@ var scaleForYScale = d3.scaleLinear().domain([30000, 3000000])
 var scaleForOtherYScales = d3.scaleLinear().domain([1200, 120000])
                     .range([400, 40000]);
 
-var constants = {
-  homePrice: 250000,
-  downRate: 20,
-  mortgageRate: 3.92,
-  mortgageYears: 30
-};
+
 
 var currentIndicators = constants;
 
 var currentPayment = getPayment(constants.homePrice, constants.mortgageYears, constants.mortgageRate, constants.downRate);
-
-// hp - home price
-// rt - mortgate rate
-// dr - down rate
-// ys - years
-function getPayment(hp, ys,  rt, dr) {
-  // down deduction
-  hp = hp * ( 1 - (dr / 100) );
-  // how many months?
-  var n = ys * 12;
-  // monthly rate
-  var i = ( rt / 100 ) / 12;
-  var d = (Math.pow((1 + i), n) - 1) / (i * Math.pow((1 + i), n));
-  var p = hp / d;
-  return p;
-}
-
 
 function renderChart(params) {
 

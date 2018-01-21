962bd2937f36382d1abc2e19acff3e618b63c9c1
unknown <mr.g.ghv@gmail.com>
2018-01-21 23:12:26 +0400

    bug fix

--- d3-alvaro-upwork/scripts/d3script.js
+++ d3-alvaro-upwork/scripts/d3script.js
@@ -38,6 +38,35 @@ function renderChart(params) {
       calc.legendHeigh = calc.chartHeight * 0.2;
       calc.lineChartHeight = calc.chartHeight * 0.8;
 
+      var t = d3.transition()
+            .duration(attrs.animationSpeed)
+            .ease(d3.easeLinear)
+            .on("start", function(d){ console.log("transiton start") })
+            .on("end", function(d){ 
+              console.log("transiton ended");
+              
+              container.selectAll(".dot")
+                       .transition(200)
+                       .ease(d3.easeLinear)
+                       .attr("opacity", 1);
+              
+            });
+
+      var line = d3.line()
+                  .x(function(d, i) { return x(i); })
+                  .y(function(d) { return y(d.value); });
+
+      // ############ scales ##############
+      var xLabels = d3.scaleTime().domain([new Date(2018, 0, 1), new Date(2018, 11, 31)]).range([0, calc.chartWidth - attrs.axisLeftWidth]);
+      var x = d3.scaleLinear().range([attrs.axisLeftWidth, calc.chartWidth]).domain([0, attrs.data[0].length]),
+          y = d3.scaleLinear().range([calc.lineChartHeight - attrs.axisBottomHeight, 0]).domain([0, d3.max(attrs.data, function(d){
+        return d3.max(d, function(x){
+          return +x.value;
+        });
+      })]);       
+      var color = d3.scaleOrdinal(d3.schemeCategory10).domain(attrs.data.map(d => { 
+        return d[0].name; 
+      }));
       //Drawing containers
       var container = d3.select(this);
       container.html('');
@@ -51,22 +80,7 @@ function renderChart(params) {
       var chart = svg.patternify({ tag: 'g', selector: 'chart' })
         .attr('transform', 'translate(' + (calc.chartLeftMargin) + ',' + calc.chartTopMargin + ')');
 
-      var xLabels = d3.scaleTime().domain([new Date(2018, 0, 1), new Date(2018, 11, 31)]).range([0, calc.chartWidth - attrs.axisLeftWidth]);
-      var x = d3.scaleLinear().range([attrs.axisLeftWidth, calc.chartWidth]).domain([0, attrs.data.length]),
-          y = d3.scaleLinear().range([calc.lineChartHeight - attrs.axisBottomHeight, 0]).domain([0, d3.max(attrs.data, function(d){
-        return +d.value;
-      })]);
-
-      var t = d3.transition()
-            .duration(attrs.animationSpeed)
-            .ease(d3.easeLinear)
-            .on("start", function(d){ console.log("transiton start") })
-            .on("end", function(d){ console.log("transiton end") });
-
-      var line = d3.line()
-                  .x(function(d, i) { return x(i); })
-                  .y(function(d) { return y(d.value); });
-
+      // ############# axes ##################
       var xAxis = chart.patternify({ tag: 'g', selector: 'axis axis--x' });
 
       xAxis.attr("transform", "translate(" + attrs.axisLeftWidth + "," + (calc.lineChartHeight - attrs.axisBottomHeight) + ")")
@@ -78,10 +92,17 @@ function renderChart(params) {
           .call(d3.axisLeft(y).ticks(5).tickSize(-calc.chartWidth)
           .tickPadding(8));
 
-      var path = chart.patternify({ tag: 'path', selector: 'line'})
-           .attr("d", line(attrs.data))
+      // ############### lines ############
+      var path = chart.patternify({ tag: 'path', selector: 'line', data: attrs.data})
+                      .attr("d", d => { return line(d); })
+                      .attr("stroke", (d, i) => {
+                        return color(d[0].name);
+                      })
+                      .attr("data-name", d => {
+                        return d[0].name;
+                      });
            
-      // animate
+      // animate if it is first load
       if (isFirstLoad){
 
         path.attr("stroke-dasharray", function(d){ return this.getTotalLength() })
@@ -89,7 +110,8 @@ function renderChart(params) {
         chart.selectAll(".line").transition(t)
             .attr("stroke-dashoffset", 0);
 
-        isFirstLoad = !isFirstLoad; // turn off animation 
+
+        isFirstLoad = !isFirstLoad; // turn off animation
       }
 
       var div = d3.select(".tooltip")
@@ -98,20 +120,34 @@ function renderChart(params) {
                   .style("color", attrs.tooltipTextColor)
                   .style("font-size", attrs.tooltipTextFontSize);
 
-      var fixeddot = chart.patternify({ tag: 'circle', selector: 'dot', data: attrs.data })
+      // ############## circles ###############
+      var circleData = [];
+      attrs.data.forEach(x => {
+        circleData = circleData.concat(x);
+      });
+
+      var fixeddot = chart.patternify({ tag: 'circle', selector: 'dot', data: circleData })
           .attr("r", 4)
           .attr("cx", function (d, i) {
-              return x(i);
+              return x(i % attrs.data[0].length);
           })
           .attr("cy", function (d) {
               return y(d.value);
           })
+          .attr("opacity", 0)
+          .attr("data-name", d => {
+            return d.name;
+          })
+          .style("fill", "#fff")
+          .attr("stroke", (d,i) => {
+            return color(d.name);
+          })
+          .attr("stroke-width", 2)
           .on("mouseover", function (d) {
                var circle = d3.select(this);
-               circle.attr("stroke", "#c3cfe2")
-                     .attr("r", 6)
-                     .attr("stroke-width", 3);
-
+               circle.style("fill", d => {
+                return color(d.name);
+               });
                div.transition()
                   .duration(100)
                   .style("opacity", .9);
@@ -120,15 +156,14 @@ function renderChart(params) {
                   .style("top", (d3.event.pageY - 28) + "px");  
           })
           .on("mouseout", function(){
-            
+
             div.style("opacity", 0);
 
             var circle = d3.select(this);
-               circle.attr("stroke", null)
-                     .attr("r", 4)
-                     .attr("stroke-width", 0);
+            circle.style("fill", "#fff");
 
-          });
+          })
+          ;
 
       var xAxisDescription = chart.patternify({ tag: 'text', selector: 'xAxisDescr' })
                                   .attr("x", calc.chartWidth / 2)
@@ -137,25 +172,70 @@ function renderChart(params) {
 
       // ##### legend #####
       var legend = chart.patternify({ tag: 'g', selector: 'legend' })
+                        .attr('transform', (d,i) => {
+                              return "translate("+ [0, calc.lineChartHeight + 50] +")"
+                        })
                         
 
-      var regUsers = legend.patternify({ tag: 'g', selector: 'registeredUsers'})
-                           .attr('transform', 'translate(10, 30)');
+      var legend_items = legend.patternify({ 
+                                         tag: 'g', 
+                                         selector: 'legend_item', 
+                                         data: attrs.data.map(d => { 
+                                            return d[0].name;
+                                         })
+                                       })
+                           .attr('transform', (d,i) => {
+                              return "translate("+ [x(i), 0] +")"
+                           });
 
-      regUsers.append("rect")
+      legend_items.append("rect")
               .attr("width", 15)
               .attr("height", 15)
-              .attr("x", 10)
-              .attr("y", calc.lineChartHeight)
+              .attr("x", (d, i) => {
+                  return x(i);
+              })
               .attr('rx', 2)
-              .attr("fill", "#2f5491");
+              .attr("fill", (d, i) => {
+                return color(d);
+              })
+              .attr("data-selected", "true")
+              .on("dblclick", function(d){
+                var rect = d3.select(this);
+                var isSelected = rect.attr("data-selected");
+                var paths = chart.selectAll(".line[data-name='"+ d +"']");
+                var dots = chart.selectAll(".dot[data-name='"+ d +"']");
+                console.log("path:", path);
+                if (isSelected == "true") {
+                  rect.style("opacity", 0.5);
+                  paths
+                    .attr("opacity", 0);
+                  dots
+                    .attr("opacity", 0);
+                  rect.attr("data-selected", "false");
+                }
+                else{
+                  rect.style("opacity", 1);
+                  paths
+                    .attr("opacity", 1);
+                  dots
+                    .attr("opacity", 1);
+                  rect.attr("data-selected", "true");
+                }
+                
+              });
 
-      regUsers.append("text")
-              .attr("x", 30)
-              .attr("y", calc.lineChartHeight + 12)
-              .attr("stroke", "#2f5491")
+      legend_items.append("text")
+              .attr("x", (d, i) => {
+                  return x(i) + 20;
+              })
+              .attr("stroke", (d, i) => {
+                return color(d);
+              })
               .attr("stroke-width", 0.9)
-              .text("Registered users");
+              .text(d => {
+                return d;
+              })
+              .attr("transform", "translate("+[0,12]+")");
 
       //RESPONSIVENESS
        d3.select(window).on('resize.' + attrs.id, function () {
@@ -172,6 +252,7 @@ function renderChart(params) {
       updateData = function () {
 
       }
+
       //#########################################  UTIL FUNCS ##################################
 
       function debug() {
@@ -231,6 +312,27 @@ function renderChart(params) {
     };
   });
 
+  // data conversion
+  function convertData(data) {
+      var arr = [];
+
+      Object.keys(data[0]).forEach(x => {
+          if (x != "month") {
+              var tempArr = [];
+              data.forEach(d => {
+                  tempArr.push({
+                      month: d.month,
+                      value: d[x],
+                      name: x
+                  });
+              });
+              arr.push(tempArr);
+          }
+      });
+
+      return arr;
+  }
+
   //Set attrs as property
   main.attrs = attrs;
 
@@ -247,7 +349,7 @@ function renderChart(params) {
   //Exposed update functions
   main.data = function (value) {
     if (!arguments.length) return attrs.data;
-    attrs.data = value;
+    attrs.data = convertData(value);
     if (typeof updateData === 'function') {
       updateData();
     }

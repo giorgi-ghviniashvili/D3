1ea925fcc5849c6d22a51ef04d32984b8be24922
unknown <mr.g.ghv@gmail.com>
2018-04-19 00:35:06 +0400

    h

--- peter-upwork/scripts/d3script.js
+++ peter-upwork/scripts/d3script.js
@@ -1,11 +1,3 @@
-/*  
-
-This code is based on following convention:
-
-https://github.com/bumbeishvili/d3-coding-conventions/blob/84b538fa99e43647d0d4717247d7b650cb9049eb/README.md
-
-
-*/
 
 function renderChart(params) {
 
